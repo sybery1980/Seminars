4040fc29a143d1d66c6f68bcba5d7a41f23572b1
vitalii zaretckii <vitaliizaretckii@MacBook-Pro-vitalii.local>
2022-11-09 23:09:22 +0300

    Lesson3-rendom massiv

---
+++ Lesson3/Program.cs
@@ -0,0 +1,14 @@
+﻿//int[] a = {120, 50, 45, 85};
+
+//for(int i = 0; i < a.Length; i++)
+//{
+  //  Console.WriteLine(a[i]);
+//}
+
+
+int[] a = new int[8];  //рендомный массив
+ for(int i = 0; i < a.Length; i++)
+ {
+    a[i] = new Random().Next(2, 4);
+    Console.Write(a[i] + " ");
+ } 

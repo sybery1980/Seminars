55e49933cab3d02f61c6ce0930a40d357846b2fc
vitalii zaretckii <vitaliizaretckii@MacBook-Pro-vitalii.local>
2022-11-09 23:18:32 +0300

    Lesson4-Homework-input massiv

---
+++ Lesson6/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+
+void setValue(int[] syb, int index)
+ {
+     Console.WriteLine("Введите значение индекса " + index);
+     syb[index] = Convert.ToInt32(Console.ReadLine());
+ }
+
+ int[] syb = new int[9];
+ for (int index = 0; index < syb.Length; index++) 
+ {
+     setValue(syb, index);
+ }
+ string output = $"{syb[0]}";
+ for (int index = 1; index < syb.Length - 1; index++)
+  {
+     output += $", {syb[index]}";
+  }
+ output += $", {syb[8]}";
+
+ Console.WriteLine("Массив: " + output);

837bca3368f0a53af8eaecf8b64acf8f560a3408
vitalii zaretckii <vitaliizaretckii@MacBook-Pro-vitalii.local>
2022-11-09 23:13:05 +0300

    Lesson4-Homework-Vozvedenie v stepen

---
+++ Lesson4/Program.cs
@@ -0,0 +1,20 @@
+﻿
+//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16 (Homework)
+
+
+
+Console.Write ("Введите A: ");
+int a = int.Parse (Console.ReadLine ()!); 
+
+
+Console.Write ("Введите B: ");
+int b = int.Parse (Console.ReadLine ()!);
+
+int syb = a;
+
+for (int i = 1; i < b; i++) 
+  syb = syb * a;
+
+ Console.WriteLine($"Число {a} в степени {b} равно {syb}");

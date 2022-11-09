6c567e18ddcefeb7a21e8d413968b44911e8790f
vitalii zaretckii <vitaliizaretckii@MacBook-Pro-vitalii.local>
2022-11-09 23:03:39 +0300

    Lesson1

---
+++ Lesson1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число");           //считаем  количество чисел в числе
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+while(number > 1)
+{
+    number = number / 10;
+    count++;
+}
+Console.WriteLine(count);

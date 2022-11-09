ab0e5ee268e7f2565407e674b272391b3d215981
vitalii zaretckii <vitaliizaretckii@MacBook-Pro-vitalii.local>
2022-11-09 23:06:56 +0300

    Lesson2-factorial

---
+++ Lesson2/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число"); //factorial
+int number = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+for(int i = 1; i <= number; i++)
+{
+    result = result * i;
+}
+Console.WriteLine(result);

d355ef188db5527de5362bc324329796b477c489
vitalii zaretckii <vitaliizaretckii@MacBook-Pro-vitalii.local>
2022-11-11 07:48:59 +0300

    Lesson8

---
+++ Lesson8/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] numbers = {-4,-8,8,2}; //massiv perevernut v otricatelnoe chislo
+
+for(int i = 0; i < numbers.Length; i++)
+{
+    numbers[i]*= -1;
+}
+WriteArray(numbers);
+void WriteArray(int[] array)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}    
+

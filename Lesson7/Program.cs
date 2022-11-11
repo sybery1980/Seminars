3db9ff76cfdf3fea52e66c259bdee3b7a7bd91b2
vitalii zaretckii <vitaliizaretckii@MacBook-Pro-vitalii.local>
2022-11-11 07:47:18 +0300

    Lesson7

---
+++ Lesson7/Program.cs
@@ -0,0 +1,30 @@
+﻿int size = 12;
+int min = -9;
+int max = 9;
+int[] numbers = new int[size];
+int positiveSum = 0;
+int negativeSum = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = new Random().Next(min, max + 1);
+}
+WriteArray(numbers);
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if(numbers[i] < 0)
+        negativeSum += numbers[i];
+    else   
+        positiveSum += numbers[i];
+}
+Console.WriteLine($"Положительная сумма = {positiveSum}, отрицательная сумма = {negativeSum}");
+
+void WriteArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}

58c8ad65768fa1ff84d81f5fecea5b7521756f44
vitalii zaretckii <vitaliizaretckii@MacBook-Pro-vitalii.local>
2022-11-11 07:53:20 +0300

    Lesson9

---
+++ Lesson9/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] numbers = {6,7,19,345,3};//pokazivaet est li chislo v massive
+Console.Write("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+for(int i = 0; i < numbers.Length; i++)
+{
+    if(numbers[i] == x)
+    {
+        Console.WriteLine("Yes");
+        return;
+    }
+    
+}
+Console.WriteLine("No");

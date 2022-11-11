e13caad0ff80ce88f848145e6fb5d6fab44f2595
vitalii zaretckii <vitaliizaretckii@MacBook-Pro-vitalii.local>
2022-11-11 07:51:43 +0300

    Lesson8.1

---
+++ Lesson8.1/Program.cs
@@ -0,0 +1,33 @@
+﻿int size = ReadInt("Введите размер массива: ");//делать массив отрицательм
+int[] numbers = new int[size];
+FillArrayRundomNumbers(numbers, -10, 10);
+WriteArray(numbers);
+
+for(int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = -numbers[i];
+}
+WriteArray(numbers);
+
+void FillArrayRundomNumbers(int[] array, int min = 1, int max = 9)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+}
+
+void WriteArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}

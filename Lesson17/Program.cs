bb30fe42f11ba4cfba5d312dda49a02f188a48a2
geekbrains <syberygames@gmail.com>
2022-11-15 16:51:33 +0300

    Задача 42

---
+++ Lesson17/Program.cs
@@ -0,0 +1,27 @@
+﻿
+//Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+//45 -> 101101
+//3  -> 11
+//2  -> 10
+
+int num = default;
+Console.Write("Введите целое число: ");
+while (!int.TryParse(Console.ReadLine(), out num))
+{
+    Console.Write("Ошибка ввода. \nВведите целое число: ");
+}
+
+int ConvertNumber(int number)
+{
+    int result = 0;
+    int count = 1;
+    while (number != 0)
+    {
+        result += (number % 2) * count;
+        number /= 2;
+        count *= 10;
+    }
+    return result;
+}
+
+Console.Write(ConvertNumber(num));

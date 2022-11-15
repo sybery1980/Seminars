3e188e988bd48c9edfbb06c6d892be58e071fe27
geekbrains <syberygames@gmail.com>
2022-11-15 16:52:33 +0300

    Задача 42.3

---
+++ Lesson17_2/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+//45 -> 101101
+//3  -> 11
+//2  -> 10
+
+int number = ReadInt("Введите десятичное число: ");  
+int tempNumber = number;
+int size = 0;
+int[] result;
+
+while(tempNumber > 0)
+{
+    size++;
+    tempNumber /= 2;
+}
+result = new int[size];
+for (int i = 0; i < result.Length; i++)
+{
+    result[result.Length - 1 - i] = number % 2;
+    number /= 2;
+}
+
+for (int i = 0; i < result.Length; i++)
+{
+    Console.Write(result[i] + " ");
+}
+
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+

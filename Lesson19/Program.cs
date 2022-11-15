a7d101a8d0e582cf3c398f49f105d1e56e64e5f0
geekbrains <syberygames@gmail.com>
2022-11-15 16:54:24 +0300

    Задача 45

---
+++ Lesson19/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 45: Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+
+
+int[] numbers = new int[8];
+int[] numbers2 = new int[8];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = new Random().Next(0, 10 + 1);
+}
+WriteArray(numbers);
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers2[i] = numbers[i];
+
+}
+WriteArray(numbers2);
+
+void WriteArray(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.WriteLine();
+}
+

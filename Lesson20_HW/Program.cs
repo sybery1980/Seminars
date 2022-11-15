d293aa46028b33d0e2264f88505b811eebdbc131
geekbrains <syberygames@gmail.com>
2022-11-15 17:47:54 +0300

    Задача 41

---
+++ Lesson20_HW/Program.cs
@@ -0,0 +1,39 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+double[] tempo (string[] strings)
+{
+    double[] numbers = new double[strings.Length];
+
+    for (int i = 0; i < strings.Length; i++)
+    {
+        numbers[i] = Convert.ToDouble(strings[i]);
+    }
+    return numbers;
+}
+int countPositive(double[] numbers)
+{
+    int count = 0;
+    foreach (var number in numbers)
+    {
+        if (number > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+Console.WriteLine("Введите некоторое количество(М) чисел, разделяя их пробелами");
+
+string[] strNumbers = Console.ReadLine().Split(" ");
+
+try
+{
+    double[] numbers = tempo(strNumbers);
+    Console.WriteLine("Количество чисел больше 0: " + countPositive(numbers));
+}
+catch (Exception e)
+{
+    Console.WriteLine("Некорректное значение");
+}

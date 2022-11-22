03248a540f6f31320ece1423aade1c181c4e873e
vitalii zaretckii <vitaliizaretckii@MacBook-Pro-vitalii.local>
2022-11-22 19:38:39 +0300

    Задача 55.1

---
+++ Lesson30.1/Program.cs
@@ -0,0 +1,49 @@
+﻿//Задача 55: Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы.
+ //В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+
+
+int rows  = ReadMessage("Введите количество строк: ");
+int columns = ReadMessage("Введите количество столбцов: ");
+int[,] numbers = new int[rows, columns];
+int[,] reverseNumbers = new int[columns, rows];
+
+Fill2DArrayRandomNumbers(numbers);
+WriteArray(numbers);
+Console.WriteLine();
+
+for (int i = 0; i < reverseNumbers.GetLength(0); i++)
+{
+    for (int j = 0; j < reverseNumbers.GetLength(1); j++)
+    {
+        reverseNumbers[i, j] = numbers[j, i];
+    }   
+}
+WriteArray(reverseNumbers);
+
+void Fill2DArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            numbers[i, j] = new Random().Next(1 , 10);
+        }   
+    }
+}
+void WriteArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int ReadMessage(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}

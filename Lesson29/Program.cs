154677b5a69e2581813303b7453eb2cc56b37138
vitalii zaretckii <vitaliizaretckii@MacBook-Pro-vitalii.local>
2022-11-22 19:35:29 +0300

    Задача 53

---
+++ Lesson29/Program.cs
@@ -0,0 +1,42 @@
+﻿
+//Задача 53: Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
+
+
+int[,] array = new int[5,5];
+FillArray(array);
+Write2DArray(array);
+Console.WriteLine();
+
+int tempNumber = 0;
+
+    for (int column = 0; column < array.GetLength(1); column++)
+    {
+        tempNumber = array[0,column];
+         array[0,column] = array[array.GetLength(0) - 1, column];
+         array[array.GetLength(0) - 1, column] = tempNumber;
+    }
+    Write2DArray(array);
+
+int[,] FillArray(int [,] array, int min = 0, int max = 10)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        
+            array[i,j] = new Random().Next(min,max+1);
+    }
+    return array;
+}
+
+void Write2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+

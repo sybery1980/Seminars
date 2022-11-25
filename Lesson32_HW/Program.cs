7160918d9f6acc214cb9f9ae045d79e3a083288b
vitalii zaretckii <vitaliizaretckii@MacBook-Pro-vitalii.local>
2022-11-25 19:44:56 +0300

    Задача 54

---
+++ Lesson32_HW/Program.cs
@@ -0,0 +1,72 @@
+﻿//Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по
+//убыванию элементы каждой строки двумерного массива.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//В итоге получается вот такой массив:
+//7 4 2 1
+//9 5 3 2
+//8 4 4 2
+
+
+
+Console.Write("Введите row: ");
+int row = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите column: ");
+int column = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите диапазон чисел");
+int range = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[row, column]; 
+CreateArray(array);
+WriteArray(array);
+
+Console.WriteLine($"\nОтсортированный массив: "); 
+OrderArrayL(array);
+WriteArray(array);
+
+void OrderArrayL(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+
+void CreateArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(range);
+        }
+    }
+}
+
+void WriteArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}

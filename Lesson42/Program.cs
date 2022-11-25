0240e9d60895d1aef3921dade747c0eb5a4f887b
vitalii zaretckii <vitaliizaretckii@MacBook-Pro-vitalii.local>
2022-11-25 19:58:50 +0300

    Задача 67

---
+++ Lesson42/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+//453 -> 12
+//45 -> 9
+
+Console.Write("Задайте число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Sum(int num) 
+{
+  if(num < 10)
+  return num;
+  else
+  return (num % 10) + Sum(num / 10);
+    
+
+}
+Console.WriteLine(Sum(number));

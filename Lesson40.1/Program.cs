13ce0a811d333cd72d7b111c66d79d48885f3b2f
vitalii zaretckii <vitaliizaretckii@MacBook-Pro-vitalii.local>
2022-11-25 19:56:53 +0300

    Задача 63.1

---
+++ Lesson40.1/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+//N = 5 -> "1, 2, 3, 4, 5"
+//N = 6 -> "1, 2, 3, 4, 5, 6"
+
+Console.Write("Задайте число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+NaturalNumber(number);
+
+void NaturalNumber(int a)
+{
+    if(a == 1)
+        Console.Write(a + " ");
+    else
+    {
+        NaturalNumber(a - 1);
+        Console.Write(a + " ");
+    }
+}

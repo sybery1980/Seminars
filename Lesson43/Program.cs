1bdccb7f742ca64cc7d69ea789a31f90d514d881
vitalii zaretckii <vitaliizaretckii@MacBook-Pro-vitalii.local>
2022-11-25 19:59:30 +0300

    Задача 69

---
+++ Lesson43/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 69: Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+//A = 3; B = 5 -> 243 (3⁵)
+//A = 2; B = 3 -> 8
+
+Console.Write("Задайте число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Задайте число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+double Power(int a, int b)
+{
+    if (b == 0)
+    return 1;
+    else if (b > 0)
+    {
+        return Power(a, b - 1) * a;
+    }
+    else
+    {
+        return 1 / Power(a, - b);
+    }
+}
+Console.WriteLine(Power(a,b));
